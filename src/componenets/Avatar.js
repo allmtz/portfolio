d03c88d3181e32c3b814a1feccfc906dd007e62d
Allan <martinezallan123@gmail.com>
2023-05-24 12:14:32 -0700

    added temporary avatar

--- src/componenets/Avatar.js
+++ src/componenets/Avatar.js
@@ -1,3 +1,10 @@
 export const Avatar = () => {
-  return <div className="card avatar-card"></div>;
+  return (
+    <div className="card avatar-card">
+      <img
+        src="https://margelo.io/assets/team/margelo_faces_taras.svg"
+        alt=""
+      />
+    </div>
+  );
 };
