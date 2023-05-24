392a5963a01fc63d136504ee5043309cf7a7db79
Allan Martinez <martinezallan123@gmail.com>
2023-05-23 22:28:28 -0700

    added more projects

--- src/componenets/Project.js
+++ src/componenets/Project.js
@@ -1,15 +1,34 @@
-function Project({ title, desc, github, demo, imageSrc, bgColor, txtColor }) {
+function Project({
+  title,
+  desc,
+  github,
+  demo,
+  imageSrc,
+  imgPosition,
+  bgColor,
+  txtColor,
+}) {
   return (
-    <div
-      className="card project-card"
-      style={{ color: `${txtColor}`, backgroundColor: `${bgColor}` }}
-    >
-      <div>
-        <p className="project-title"> {title}</p>
-        <p className="project-desc">{desc}</p>
+    <a href={demo} target="_blank" rel="noreferrer">
+      <div
+        className="card project-card"
+        style={{
+          color: `${txtColor}`,
+          backgroundColor: `${bgColor}`,
+        }}
+      >
+        <div>
+          <p className="project-title"> {title}</p>
+          <p className="project-desc">{desc}</p>
+        </div>
+        <img
+          className="project-image"
+          src={imageSrc}
+          alt="landing page"
+          style={{ objectPosition: `${imgPosition}` }}
+        />
       </div>
-      <img className="project-image" src={imageSrc} alt="" />
-    </div>
+    </a>
   );
 }
 
--- src/componenets/Projects.js
+++ src/componenets/Projects.js
@@ -3,6 +3,10 @@ import { Project } from "../componenets/Project";
 // project images
 import scoot from "../project-images/scoot.png";
 import movieApp from "../project-images/entertainment-app.png";
+import gallery from "../project-images/galleria-slideshow.png";
+import kanban from "../project-images/task-tracker.png";
+import pomodoro from "../project-images/pomodoro.png";
+import hk from "../project-images/hk.png";
 
 export const Projects = () => {
   return (
@@ -13,11 +17,11 @@ export const Projects = () => {
           "Multi-page fully responsive mock scooter rental website. Custom design built with React, Next.js, TypeScript and TailwindCSS."
         }
         imageSrc={scoot}
-        github={""}
-        demo={""}
-        // bgColor={"#455574"}
+        github={"https://github.com/allmtz/scoot"}
+        demo={"https://scoot-phi.vercel.app/"}
         bgColor={"white"}
         txtColor={"black"}
+        imgPosition={"left"}
       />
 
       <Project
@@ -26,10 +30,61 @@ export const Projects = () => {
           "Entertainment app built with React and TMDB API. User can search movies, bookmark items or get more information by clicking on an item."
         }
         imageSrc={movieApp}
-        github={""}
-        demo={""}
+        github={"https://github.com/allmtz/entertainment-web-app"}
+        demo={"https://entertainment-app-4ce2a.web.app/"}
         bgColor={"#0a192f"}
         txtColor={"whitesmoke"}
+        imgPosition={"left"}
+      />
+
+      <Project
+        title={"Art Gallery"}
+        desc={
+          "A modern gallery built with a responsive mobile-first approach. Includes a user controlled slideshow feature."
+        }
+        imageSrc={gallery}
+        github={"https://github.com/allmtz/galleria-slideshow"}
+        demo={"https://imaginative-cajeta-48538a.netlify.app/"}
+        bgColor={"white"}
+        txtColor={"black"}
+        imgPosition={"left"}
+      />
+
+      <Project
+        title={"Kanban Task Tracker"}
+        desc={
+          "This Kanban task tracker is a fully responsive React application and includes a light/dark toggle."
+        }
+        imageSrc={kanban}
+        github={"https://github.com/allmtz/task-tracker"}
+        demo={"https://task-tracker-9966c.web.app/"}
+        bgColor={"orange"}
+        txtColor={"black"}
+        imgPosition={"left"}
+      />
+
+      <Project
+        title={"Pomodoro"}
+        desc={
+          "A pomodoro style productivity timer. The font and color are customizable and the progress bar provides a quick visual of the time remaining"
+        }
+        imageSrc={pomodoro}
+        github={"https://github.com/allmtz/Pomodoro-App"}
+        demo={"https://majestic-cuchufli-f2af29.netlify.app/"}
+        bgColor={"orange"}
+        txtColor={"black"}
+        imgPosition={"center"}
+      />
+
+      <Project
+        title={"Hotkey Checker"}
+        desc={"a tool"}
+        imageSrc={hk}
+        github={"https://github.com/allmtz/hotkey-checker"}
+        demo={"https://hotkey-checker.vercel.app/"}
+        bgColor={"white"}
+        txtColor={"black"}
+        imgPosition={"center"}
       />
     </>
   );
